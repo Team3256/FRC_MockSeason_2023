8d8689ddcf3a30ef213c2c631680b62a566c25ff
ThatXliner <bryan.hu.2020@gmail.com>
2023-12-01 16:00:57 -0800

    :rotating_light: Fix lints

--- src/main/java/frc/robot/utils/CANConnectionManager.java
+++ src/main/java/frc/robot/utils/CANConnectionManager.java
@@ -1,52 +1,55 @@
-package frc.robot.utils;
+// Copyright (c) 2023 FRC 3256
+// https://github.com/Team3256
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file at
+// the root directory of this project.
 
-import java.util.ArrayList; // import the ArrayList class
+package frc.robot.utils;
 
 import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.Pigeon2;
 import com.ctre.phoenix6.hardware.TalonFX;
-
 import edu.wpi.first.wpilibj.PowerDistribution;
+import java.util.ArrayList; // import the ArrayList class
 
 public class CANConnectionManager {
-    private ArrayList<CANTestable> devices = new ArrayList<CANTestable>();
+  private ArrayList<CANTestable> devices = new ArrayList<CANTestable>();
 
-    public CANConnectionManager(ArrayList<CANTestable> initialDevices) {
-        devices = initialDevices;
+  public CANConnectionManager(ArrayList<CANTestable> initialDevices) {
+    devices = initialDevices;
+  }
 
-    }
+  public CANConnectionManager() {}
 
-    public CANConnectionManager() {
-    }
+  public void add(CANTestable device) {
+    devices.add(device);
+  }
 
-    public void add(CANTestable device) {
-        devices.add(device);
-    }
-
-    /*
-     * Returns the first broken device in the CAN daisy chain or null if all devices
-     * are online
-     */
-    public CANTestable getBrokenDevice() {
-        for (CANTestable device : devices) {
-            if (device instanceof PowerDistribution) {
-                if (!CANDeviceTester.testPDP((PowerDistribution) device)) {
-                    return device;
-                }
-            } else if (device instanceof TalonFX) {
-                if (!CANDeviceTester.testTalonFX((TalonFX) device)) {
-                    return device;
-                }
-            } else if (device instanceof Pigeon2) {
-                if (!CANDeviceTester.testPigeon((Pigeon2) device)) {
-                    return device;
-                }
-            } else if (device instanceof CANcoder) {
-                if (!CANDeviceTester.testCANCoder((CANcoder) device)) {
-                    return device;
-                }
-            }
+  /*
+   * Returns the first broken device in the CAN daisy chain or null if all devices
+   * are online
+   */
+  public CANTestable getBrokenDevice() {
+    for (CANTestable device : devices) {
+      if (device instanceof PowerDistribution) {
+        if (!CANDeviceTester.testPDP((PowerDistribution) device)) {
+          return device;
         }
-        return null;
+      } else if (device instanceof TalonFX) {
+        if (!CANDeviceTester.testTalonFX((TalonFX) device)) {
+          return device;
+        }
+      } else if (device instanceof Pigeon2) {
+        if (!CANDeviceTester.testPigeon((Pigeon2) device)) {
+          return device;
+        }
+      } else if (device instanceof CANcoder) {
+        if (!CANDeviceTester.testCANCoder((CANcoder) device)) {
+          return device;
+        }
+      }
     }
+    return null;
+  }
 }
--- src/main/java/frc/robot/utils/CANTestable.java
+++ src/main/java/frc/robot/utils/CANTestable.java
@@ -1,6 +1,12 @@
+// Copyright (c) 2023 FRC 3256
+// https://github.com/Team3256
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file at
+// the root directory of this project.
+
 package frc.robot.utils;
 
 /* Basically a type union between TalonFX, Pigeon2, CANcoder, and PowerDistribution
  */
-public interface CANTestable {
-}
+public interface CANTestable {}
